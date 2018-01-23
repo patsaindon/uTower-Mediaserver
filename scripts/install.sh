34b9edcadd93c4d5d19f76a9086fea732ee4c7bb
Patrick Saindon <pat.saindon@gmail.com>
2018-01-23 10:41:43 -0500

    Added containers

---
+++ scripts/install.sh
@@ -0,0 +1,18 @@
+echo -e "\n\033[0;32m >> Install Requirements\033[0m"
+sudo apt-get -y update 2>&1 >/dev/null
+sudo /usr/bin/apt-get -y install git python-pip
+# sudo /usr/bin/pip install -U ansible prompter
+sudo /usr/bin/pip install -U ansible
+
+
+if [ ! -d "$HOME/ansible-uTower-roles" ]; then
+	echo -e  "\n\033[0;32m >> Clone  ansible-uTower-roles repository\033[0m"
+	git clone --recursive git://github.com/Patsaindon/ansible-uTower-roles.git "$HOME/ansible-uTower-roles"
+else
+	echo -e "\n\033[0;32m >> ansible-uTower-roles is already available\033[0m"
+fi
+cd "$HOME/ansible-uTower-roles"
+# echo -e "\n\033[0;32m >> Run Wizard\033[0m"
+# python scripts/wizard.py <&1
+echo -e "\n\033[0;32m >> Installing ...\033[0m"
+ansible-playbook -i inventory/server-withclient -c local -K uTower-site.yml
