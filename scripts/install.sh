1dd62d19cd9f96a9b8c131c4411350904ec22a4d
Patrick Saindon <pat.saindon@gmail.com>
2018-02-01 09:23:54 -0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,4 +16,4 @@ cd "$HOME/ansible-uTower-roles"
 # echo -e "\n\033[0;32m >> Run Wizard\033[0m"
 # python scripts/wizard.py <&1
 echo -e "\n\033[0;32m >> Installing ...\033[0m"
-ansible-playbook -i inventory/inventory -c local -K site.yml
+ansible-playbook -i inventory/inventory.yml -c local -K site.yml
